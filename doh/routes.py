1d4bd919f6b66e1ecdd9cc1834515de5b7db9b8c
sarmadna <sarmad_n_a@hotmail.com>
2023-02-24 13:06:03 +0300

    update

--- doh/routes.py
+++ doh/routes.py
@@ -5,6 +5,22 @@ from flask import render_template
 def main():
     return render_template('main.html', title='Main')
 
+@app.route('/departments')
+def departments():
+    return render_template('departments.html', title='Departments')
+
+@app.route('/directorates')
+def directorates():
+    return render_template('directorates.html', title='Directorates')
+
+@app.route('/forms')
+def forms():
+    return render_template('forms.html', title='Forms')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html', title='Registration')
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
